1e5337f2ec39dedc6695ef8ab76f79da35d5a674
João Dias <jptdias10@gmail.com>
2026-02-05 12:56:49 -0300

    [feat]: add html route

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200)
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def read_root_html():
+    return b"""
+    <html>
+        <head>
+            <title>First Hello World!</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,20 @@ def test_read_root_must_return_hello_world():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World!'}
+
+
+def test_read_root_must_return_hello_world_html():
+    response = client.get('/html')
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.content
+        == b"""
+    <html>
+        <head>
+            <title>First Hello World!</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>"""
+    )

a9289611fe296113adfc8793b12bfdcf5573d283
João Dias <jptdias10@gmail.com>
2026-02-05 13:07:27 -0300

    [feat]: add schema

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,14 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/', status_code=200)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World!'}
 

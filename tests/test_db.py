d518e153cc97c5627683e5caddbc570329dfd06b
João Dias <jptdias10@gmail.com>
2026-02-06 13:42:15 -0300

    [feat]: test to db

---
+++ tests/test_db.py
@@ -0,0 +1,28 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='testuser',
+            email='testuser@example.com',
+            password='password',
+        )
+
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'testuser'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'testuser@example.com',
+        'password': 'password',
+        'created_at': time,
+    }
